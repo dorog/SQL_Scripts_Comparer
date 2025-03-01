36dce127ff3c94dd01c776397857d2afbc7c4722
dorog <dorogi.x@gmail.com>
2025-03-01 14:44:43 +0100

    Add variables to use them for comparing

---
+++ SqlScriptsComparer/SQL/SqlScriptsComparer.cs
@@ -0,0 +1,10 @@
+﻿namespace SQL;
+
+public static class SqlScriptsComparer
+{
+    public static string CompareSqlScripts(string leftSqlScript, string rightSqlScript)
+    {
+        // Placeholder
+        return leftSqlScript + "\n\n" + rightSqlScript;
+    }
+}
