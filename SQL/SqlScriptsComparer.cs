1e7c098225a8ce57d308dd00082f123b54793316
dorog <dorogi.x@gmail.com>
2025-03-09 13:58:49 +0100

    Add class library for business logic

---
+++ SQL/SqlScriptsComparer.cs
@@ -0,0 +1,9 @@
+﻿namespace SQL;
+
+public static class SqlScriptsComparer
+{
+    public static string CompareSqlScripts(string leftSqlScript, string rightSqlScript)
+    {
+        return "Result: " + leftSqlScript + " " + rightSqlScript;
+    }
+}
